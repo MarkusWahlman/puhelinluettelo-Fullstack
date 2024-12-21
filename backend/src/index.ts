0bffaac924d89fe5d889fa66170ec9cf8c1b9998
Markus Wahlman <wahlman2002@gmail.com>
2024-12-21 17:27:18 +0200

    More tasks...

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.static("../frontend/dist"));
 
 app.use("/api", apiRouter);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
